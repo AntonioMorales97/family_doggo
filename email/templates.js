b4a167b80cdb9e0b1d011b04275b19e7086319c4
Antonio <antonio_97@live.se>
2019-06-14 16:29:52 +0200

    added forgot password api to backend

--- email/templates.js
+++ email/templates.js
@@ -2,7 +2,7 @@ const { CLIENT_ORIGIN } = require('../config/config');
 
 module.exports = {
   confirm: token => ({
-    subject: 'React Confirm Email',
+    subject: 'Confirm Email',
     html: `
     <h1>Hello,</h1>
           <a href='${CLIENT_ORIGIN}/confirm/${token}'>
@@ -10,5 +10,29 @@ module.exports = {
           </a>
         `,
     text: `Copy and paste this link: ${CLIENT_ORIGIN}/confirm/${token}`
+  }),
+  resetPassword: token => ({
+    subject: 'Reset Password',
+    html: `
+    <h1>Hello,</h1>
+          <p>This mail has been sent to you because you (or someone else) have 
+          requested to reset your password. <strong>If this was not you, please ignore this email.</strong></p>
+          <a href='${CLIENT_ORIGIN}/reset/${token}'>
+            Click to reset your password
+          </a>
+        `,
+    text: `Copy and paste this link: ${CLIENT_ORIGIN}/reset/${token}`
+  }),
+  resetPasswordConfirmation: email => ({
+    subject: 'Reset Password Confirmation',
+    html: `
+    <h1>Hello,</h1>
+          <p>This mail has been sent to you as a confirmation that the password for 
+          the account ${email} has been successfully changed.</p>
+          <a href='${CLIENT_ORIGIN}/login'>
+            Click to login with your new password
+          </a>
+        `,
+    text: `Copy and paste this link: ${CLIENT_ORIGIN}/login`
   })
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
-const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const {
   userRateLimiter,
@@ -19,6 +18,7 @@ const VerificationToken = require('../../models/VerificationToken');
 
 // Constants
 const verificationExpiration = config.get('VERIFICATION_EXPIRATION');
+const resetPasswordExpiration = config.get('RESET_PASSWORD_EXPIRATION_MS');
 
 // @route   POST api/users
 // @desc    Register new user and wait for verification
@@ -27,9 +27,13 @@ router.post('/', [userRateLimiter, userSlowDown], (req, res) => {
   const { name, email, password } = req.body;
 
   // Simple validation
-  if (!name || !email || !password) {
+  if (!name || !email || !password)
     return res.status(400).json({ msg: 'Please enter all fields' });
-  }
+
+  if (password.length < 6)
+    return res
+      .status(400)
+      .json({ msg: 'Password must be longer than 6 characters!' });
 
   // Check for existing user
   User.findOne({ email }).then(user => {
@@ -128,5 +132,90 @@ router.get(
     });
   }
 );
+
+// @route   GET api/users/forgot
+// @desc    For users that have forgotten their password.
+//          This will create a resetPassword-token and send an email
+//          to the user if the email exists
+// @access  Public
+router.post('/forgot', [userRateLimiter, userSlowDown], (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ msg: 'Please enter an email' });
+  }
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(400).json({ msg: 'User does not exist' });
+    } else if (user.passwordResetExpires > Date.now()) {
+      return res
+        .status(400)
+        .json({ msg: 'An email has already been sent to that email address!' });
+    }
+
+    user.passwordResetToken = crypto.randomBytes(10).toString('hex');
+    user.passwordResetExpires = Date.now() + resetPasswordExpiration;
+
+    user.save().then(user => {
+      sendEmail(email, templates.resetPassword(user.passwordResetToken))
+        .then(() =>
+          res.status(200).json({
+            msg: `An email has been sent to ${email} with instructions how to reset your password`
+          })
+        )
+        .catch(err => res.status(500).json({ msg: err.message }));
+    });
+  });
+});
+
+// @route   GET api/users/reset/:token
+// @desc    Reset of password
+// @access  Public
+router.post('/reset/:token', [userRateLimiter, userSlowDown], (req, res) => {
+  User.findOne({
+    passwordResetToken: req.params.token,
+    passwordResetExpires: { $gt: Date.now() }
+  }).then(user => {
+    if (!user)
+      return res.status(400).json({
+        msg: 'The password reset token is invalid or has already expired'
+      });
+
+    const { password, confirmPassword } = req.body;
+    if (!password || !confirmPassword)
+      return res.status(400).json({ msg: 'Please enter all fields!' });
+
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ msg: 'Password must be longer than 6 characters!' });
+
+    if (password !== confirmPassword)
+      return res.status(400).json({ msg: 'Passwords do not match!' });
+
+    // Create salt and hash
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return res.status(500).json({ msg: err.message });
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) return res.status(500).json({ msg: err.message });
+        user.password = hash; //set hash
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        // Save user
+        user.save().then(user => {
+          sendEmail(user.email, templates.resetPasswordConfirmation(user.email))
+            .then(() =>
+              res.status(200).json({
+                msg: `Password changed successfully. An email confirmation has been sent to ${
+                  user.email
+                }`
+              })
+            )
+            .catch(err => res.status(500).json({ msg: err.message }));
+        });
+      });
+    });
+  });
+});
 
 module.exports = router;
