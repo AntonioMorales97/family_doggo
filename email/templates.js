b98f00059c32635dbbf1e0c48cfdc722cc418c6e
Antonio <antonio_97@live.se>
2019-06-26 15:05:52 +0200

    added family-invitation and family-join-confirmation templates

--- email/templates.js
+++ email/templates.js
@@ -2,7 +2,7 @@ const { CLIENT_ORIGIN } = require('../config/config');
 
 module.exports = {
   confirm: (token, expirationHours) => ({
-    subject: 'Account verification',
+    subject: 'Account Verification',
     html: `
     <h1>Hello,</h1>
     <p>This mail has been sent to you so you can verify your email address which you provided for us. If 
@@ -34,6 +34,30 @@ module.exports = {
           the account ${email} has been successfully changed.</p>
           <a href='${CLIENT_ORIGIN}/login'>
             Click to login with your new password
+          </a>
+        `,
+    text: `Copy and paste this link: ${CLIENT_ORIGIN}/login`
+  }),
+  invitationToFamily: (token, invitor, expirationHours) => ({
+    subject: 'Family Invitation',
+    html: `
+    <h1>Hello, </h1>
+      <p>This mail has been sent to you because ${invitor} has invited you to join his/her family so you can share walks with your dogs.
+      If you don't want to join the family, simply ignore this mail. The link will expire 
+      in ${expirationHours} hours. <strong>Please note that you cannot have a family registered if you wish to join a new family.</strong></p>
+          <a href='${CLIENT_ORIGIN}/join-family/${token}'>
+            Click to join the family!
+          </a>
+          `,
+    text: `Copy and paste this link: ${CLIENT_ORIGIN}/join-family/${token}`
+  }),
+  familyJoinConfirmation: (name, familyName) => ({
+    subject: 'Successfully Joined Family',
+    html: `
+    <h1>Hello ${name},</h1>
+          <p>This mail has been sent to you as a confirmation that you have successfully joined the ${familyName} family!</p>
+          <a href='${CLIENT_ORIGIN}/login'>
+            Click to login and start adding walks!
           </a>
         `,
     text: `Copy and paste this link: ${CLIENT_ORIGIN}/login`
