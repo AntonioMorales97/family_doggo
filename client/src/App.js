189dea51d0e13a3f7e6a93b35555ac8aa568e52b
Antonio <antonio_97@live.se>
2019-08-20 14:14:00 +0200

    moved styles to index.js and installed jquery and popper.js (needed for full bootstrap)

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/authActions';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss//App.scss';
-
 class App extends Component {
   componentDidMount() {
     store.dispatch(loadUser());
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// eslint-disable-next-line
+import $ from 'jquery';
+// eslint-disable-next-line
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/App.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
