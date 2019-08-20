04a1a36aef52cef039120c8b1414981c4db468bf
Antonio <antonio_97@live.se>
2019-08-20 12:19:01 +0200

    forgot one zero

--- middleware/limitation.js
+++ middleware/limitation.js
@@ -3,7 +3,7 @@ const slowDown = require('express-slow-down');
 
 // Limiters for api/users
 const userRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 100,
+  windowMs: 15 * 60 * 1000,
   max: 5,
   message: {
     msg: 'Too many requests, please try again later'
@@ -11,14 +11,14 @@ const userRateLimiter = rateLimit({
 });
 
 const userSlowDown = slowDown({
-  windowMs: 15 * 60 * 100,
+  windowMs: 15 * 60 * 1000,
   delayAfter: 1,
   delayMs: 300
 });
 
 // Limiters for api/auth
 const loginRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 100,
+  windowMs: 15 * 60 * 1000,
   max: 5,
   message: {
     msg: 'Too many attempts, please try again later'
@@ -26,13 +26,13 @@ const loginRateLimiter = rateLimit({
 });
 
 const loginSlowDown = slowDown({
-  windowMs: 15 * 60 * 100,
+  windowMs: 15 * 60 * 1000,
   delayAfter: 1,
   delayMs: 300
 });
 
 const getUserSlowDown = slowDown({
-  windowMs: 5 * 60 * 100,
+  windowMs: 5 * 60 * 1000,
   delayAfter: 10,
   delayMs: 100,
   maxDelayMs: 1000
